3343c16aa8472c5c5f008bddfc3cc2ccb5c4c127
grcsania <gracesania9@gmail.com>
2026-03-23 17:50:44 +0700

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 val koin_version = "4.1.2-Beta1"
 val ktor_version = "3.0.3"
-val kotlin_version = "2.1.0"
+// KITA NAIKKAN KOTLINNYA BIAR KUAT NAHAN JAVA 25
+val kotlin_version = "2.1.20"
 val logback_version = "1.4.14"
 val dotenv_version = "6.4.1"
 
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.1.20"
     id("io.ktor.plugin") version "3.0.3"
-    kotlin("plugin.serialization") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 group = "org.delcom"
@@ -17,24 +18,22 @@ application {
     mainClass.set("org.delcom.ApplicationKt")
 }
 
-// SURAT PERINTAH TEGAS UNTUK KOTLIN & JAVA (Biar lokal & server Delcom nurut)
+// YAUDAH KITA KASIH SERVER APA YANG DIA MAU: JAVA 25!
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.toVersion("25")
+    targetCompatibility = JavaVersion.toVersion("25")
 }
 
 dependencies {
-    // Ktor Core & Server
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
 
-    // Ktor Plugins (Ini yang memperbaiki error JSON kamu)
     implementation("io.ktor:ktor-server-cors:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
@@ -42,11 +41,9 @@ dependencies {
     implementation("io.ktor:ktor-server-host-common:$ktor_version")
     implementation("io.ktor:ktor-server-status-pages:$ktor_version")
 
-    // Auth & JWT
     implementation("io.ktor:ktor-server-auth:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
 
-    // Database
     implementation("org.postgresql:postgresql:42.7.9")
     implementation("com.h2database:h2:2.3.232")
     implementation("org.jetbrains.exposed:exposed-core:0.61.0")
@@ -54,13 +51,11 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
     implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.61.0")
 
-    // Koin & Utils
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
     implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
     implementation("org.mindrot:jbcrypt:0.4")
 
-    // Testing
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
