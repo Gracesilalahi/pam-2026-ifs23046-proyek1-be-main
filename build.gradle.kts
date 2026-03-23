ce68946d93aafb1bd0d860b88c7a43f2a8f94dae
grcsania <gracesania9@gmail.com>
2026-03-23 17:04:38 +0700

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
-val koin_version: String = "4.1.2-Beta1"
-val ktor_version: String = "3.0.3"
-val kotlin_version: String = "2.1.0"
-val logback_version: String = "1.4.14"
-val dotenv_version: String = "6.4.1"
+// KITA KUNCI VERSINYA SECARA PAKSA DI SINI (Hapus 'by project')
+val koin_version = "4.0.0"
+val ktor_version = "3.0.3"
+val kotlin_version = "2.1.0"
+val logback_version = "1.4.14"
+val dotenv_version = "6.4.1"
 
 plugins {
     kotlin("jvm") version "2.1.0"
@@ -17,37 +18,44 @@ application {
     mainClass.set("org.delcom.ApplicationKt")
 }
 
-dependencies {
-    // Ktor Server Core & Netty
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+// Surat perintah untuk KOTLIN
+kotlin {
+    jvmToolchain(21)
+}
 
-    // Plugins
+// Surat perintah untuk JAVA
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+dependencies {
+    // SEMUA KTOR KITA KUNCI KE ktor_version (3.0.3)
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
     implementation("io.ktor:ktor-server-cors:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
     implementation("io.ktor:ktor-server-auth:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
-    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common:$ktor_version")
 
-    // Database (Exposed & Postgres)
-    implementation("org.postgresql:postgresql:42.7.3")
+    // Database & Koin dll
+    implementation("org.postgresql:postgresql:42.7.9")
+    implementation("com.h2database:h2:2.3.232")
     implementation("org.jetbrains.exposed:exposed-core:0.61.0")
     implementation("org.jetbrains.exposed:exposed-dao:0.61.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
     implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.61.0")
-
-    // Koin Injection
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
-
-    // Utils
     implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
     implementation("org.mindrot:jbcrypt:0.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
 
     // Testing
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
